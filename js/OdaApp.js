65beb911349e1438f6fcba248a3bb17fd4829468
happykiller <fabrice.rosito@gmail.com>
2017-01-19 14:41:00 +0100

    update

--- js/OdaApp.js
+++ js/OdaApp.js
@@ -62,13 +62,16 @@
         "Controller" : {
             BonitaSession: {},
             BonitaActivities: [],
+            BonitaActivitiesObjTable: [],
             BonitaActivitiesFilters: {},
+            BonitaActivitiesTable: {},
             "Home": {
                 /**
                  * @returns {$.Oda.App.Controller.Home}
                  */
                 start: function () {
                     try {
+                        $.Oda.App.Controller.BonitaSession = $.Oda.Storage.get("bonitaSession", {});
                         if($.Oda.App.Controller.BonitaSession.apiToken === undefined){
                             var strHtml = $.Oda.Display.TemplateHtml.create({
                                 template : "tlpDivLogBonita"
@@ -118,6 +121,7 @@
                                     success: function(data, textStatus, jqXHR) {
                                         $.Oda.App.Controller.BonitaSession.apiToken = jqXHR.getResponseHeader('X-Bonita-API-Token');
                                         $.Oda.App.Controller.BonitaSession.sessionInfo = data;
+                                        $.Oda.Storage.set("bonitaSession", $.Oda.App.Controller.BonitaSession, 3600);
                                         $.Oda.App.Controller.Home.drawFormActivities();
                                     }
                                 });
@@ -155,6 +159,8 @@
                                     }else{
                                         $("#submit").btDisable();
                                         $("#divTabActivities").html("");
+                                        $("#divMenuReports").html("");
+                                        $("#divReport").html("");
                                     }
                                 }
                             });
@@ -169,6 +175,8 @@
                  */
                 displayActivities: function () {
                     try {
+                        $("#divMenuReports").html("");
+                        $("#divReport").html("");
                         var startDate = $("#startDate").val();
                         var endDate = $("#endDate").val();
                         $.Oda.Display.loading({elt :$("#divTabActivities")});
@@ -185,24 +193,24 @@
                                 }
 
                                 $.Oda.App.Controller.BonitaActivities = data;
-                                var objDataTable = $.Oda.Tooling.objDataTableFromJsonArray(data);
+                                $.Oda.App.Controller.BonitaActivitiesObjTable = $.Oda.Tooling.objDataTableFromJsonArray(data);
                                 var strhtml = '<table cellpadding="0" cellspacing="0" border="0" class="table table-striped table-bordered hover" id="tabActivities">';
                                 strhtml += '<tfoot><tr>'; 
                                 strhtml += '<th oda-attr-value="customer"></th>'; 
                                 strhtml += '<th oda-attr="select" oda-attr-value="category"></th>'; 
+                                strhtml += '<th oda-attr-value="subcategory"></th>'; 
                                 strhtml += '<th oda-attr="none"></th>'; 
-                                strhtml += '<th oda-attr="none"></th>'; 
-                                strhtml += '<th oda-attr="none"></th>'; 
+                                strhtml += '<th oda-attr="sum" oda-attr-value="workHours"></th>'; 
                                 strhtml += '<th oda-attr="select" oda-attr-value="location"></th>'; 
-                                strhtml += '<th oda-attr="none"></th>'; 
+                                strhtml += '<th oda-attr-value="consultant"></th>'; 
                                 strhtml += '<th oda-attr-value="description"></th>'; 
                                 strhtml += '<th oda-attr-value="customerDescription"></th>'; 
                                 strhtml += '</tr></tfoot></table>';
                                 $('#divTabActivities').html(strhtml);
 
-                                var oTable = $('#tabActivities').dataTable({
+                                $.Oda.App.Controller.BonitaActivitiesTable = $('#tabActivities').DataTable({
                                     "sPaginationType": "full_numbers",
-                                    "aaData": objDataTable.data,
+                                    "aaData": $.Oda.App.Controller.BonitaActivitiesObjTable.data,
                                     "aaSorting": [[3, 'desc']],
                                     "aoColumns": [
                                         {"sTitle": "customer"},
@@ -218,82 +226,69 @@
                                     "aoColumnDefs": [
                                         {
                                             "mRender": function (data, type, row) {
-                                                return row[objDataTable.entete["customer"]].name;
+                                                return row[$.Oda.App.Controller.BonitaActivitiesObjTable.entete["customer"]].name;
                                             },
                                             "aTargets": [0]
                                         },
                                         {
                                             "mRender": function (data, type, row) {
-                                                return row[objDataTable.entete["category"]];
+                                                return row[$.Oda.App.Controller.BonitaActivitiesObjTable.entete["category"]];
                                             },
                                             "aTargets": [1]
                                         },
                                         {
                                             "mRender": function (data, type, row) {
-                                                return row[objDataTable.entete["subcategory"]];
+                                                return row[$.Oda.App.Controller.BonitaActivitiesObjTable.entete["subcategory"]];
                                             },
                                             "aTargets": [2]
                                         },
                                         {
                                             "mRender": function (data, type, row) {
                                                 if (type === 'display') {
-                                                    return $.Oda.Date.dateFormat(row[objDataTable.entete["workDate"]], 'dd/mm/yyyy');
+                                                    return $.Oda.Date.dateFormat(row[$.Oda.App.Controller.BonitaActivitiesObjTable.entete["workDate"]], 'dd/mm/yyyy');
                                                 }else{
-                                                    return row[objDataTable.entete["workDate"]];
+                                                    return row[$.Oda.App.Controller.BonitaActivitiesObjTable.entete["workDate"]];
                                                 }
                                             },
                                             "aTargets": [3]
                                         },
                                         {
                                             "mRender": function (data, type, row) {
-                                                return row[objDataTable.entete["workHours"]];
+                                                return row[$.Oda.App.Controller.BonitaActivitiesObjTable.entete["workHours"]];
                                             },
                                             "aTargets": [4]
                                         },
                                         {
                                             "mRender": function (data, type, row) {
-                                                return row[objDataTable.entete["location"]];
+                                                return row[$.Oda.App.Controller.BonitaActivitiesObjTable.entete["location"]];
                                             },
                                             "aTargets": [5]
                                         },
                                         {
                                             "mRender": function (data, type, row) {
-                                                return row[objDataTable.entete["consultant"]];
+                                                return row[$.Oda.App.Controller.BonitaActivitiesObjTable.entete["consultant"]];
                                             },
                                             "aTargets": [6]
                                         },
                                         {
                                             "mRender": function (data, type, row) {
-                                                return row[objDataTable.entete["description"]];
+                                                return row[$.Oda.App.Controller.BonitaActivitiesObjTable.entete["description"]];
                                             },
                                             "aTargets": [7]
                                         },
                                         {
                                             "mRender": function (data, type, row) {
-                                                return row[objDataTable.entete["customerDescription"]];
+                                                return row[$.Oda.App.Controller.BonitaActivitiesObjTable.entete["customerDescription"]];
                                             },
                                             "aTargets": [8]
                                         }
                                     ]
                                 });
 
-                                // DataTable
-                                var table = $('#tabActivities').DataTable();
-
-                                $('#tabActivities tbody').on('click', 'tr', function () {
-                                    if ($(this).hasClass('selected')) {
-                                        $(this).removeClass('selected');
-                                    }
-                                    else {
-                                        table.$('tr.selected').removeClass('selected');
-                                        $(this).addClass('selected');
-                                    }
-                                });
-
                                 $("#tabActivities tfoot th").each(function (i) {
                                     var valOdaAttri = $(this).attr("oda-attr");
                                     var odaAttriValue = $(this).attr("oda-attr-value");
-                                    var indexColl = objDataTable.entete[odaAttriValue];
+                                    var indexColl = $.Oda.App.Controller.BonitaActivitiesObjTable.entete[odaAttriValue];
                                     if(odaAttriValue !== undefined){
                                         $.Oda.App.Controller.BonitaActivitiesFilters[odaAttriValue] = "";
                                     }
@@ -304,12 +299,13 @@
                                                 var odaAttriValue = $(this).attr("oda-filter");
                                                 var val = $(this).val();
                                                 $.Oda.App.Controller.BonitaActivitiesFilters[odaAttriValue] = val;
-                                                table.column(i)
+                                                $.Oda.App.Controller.BonitaActivitiesTable.column(i)
                                                     .search(val ? '^' + $(this).val() + '$' : val, true, false)
                                                     .draw();
+                                                $.Oda.App.Controller.Home.displayTotal();
                                             });
 
-                                        var coll = table.column(indexColl);
+                                        var coll = $.Oda.App.Controller.BonitaActivitiesTable.column(indexColl);
                                         try{
                                             var datas = coll.data().unique().sort();
                                             datas.each(function (d, j) {
@@ -319,6 +315,9 @@
                                             $(this).empty()
                                             console.error('error for build list with the coll:'+odaAttriValue);
                                         }
+                                    } else if (valOdaAttri === "sum") {
+                                        $(this).html("<span id='totalDays'></span>");
+                                        $.Oda.App.Controller.Home.displayTotal();
                                     } else if (valOdaAttri !== "none") {
                                         $('<input oda-filter="'+odaAttriValue+'" type="text" placeholder="Search" size="4"/>')
                                             .appendTo($(this).empty())
@@ -326,10 +325,11 @@
                                                 var odaAttriValue = $(this).attr("oda-filter");
                                                 var val = $(this).val();
                                                 $.Oda.App.Controller.BonitaActivitiesFilters[odaAttriValue] = val;
-                                                table
+                                                $.Oda.App.Controller.BonitaActivitiesTable
                                                     .column(i)
                                                     .search(val)
                                                     .draw();
+                                                $.Oda.App.Controller.Home.displayTotal();
                                             });
                                     } else {
                                         $(this).empty();
@@ -364,8 +364,13 @@
                 /**
                  * @returns {$.Oda.App.Controller.Home}
                  */
-                displayReport: function () {
+                displayReportCamCategory: function () {
                     try {
+                        var strHtml = $.Oda.Display.TemplateHtml.create({
+                            template : "tlpDivReportCamCategory"
+                        });
+                        $.Oda.Display.render({id:"divReport", html: strHtml});
+
                         var strFilters = "";
                         for(var key in $.Oda.App.Controller.BonitaActivitiesFilters){
                             var filter = $.Oda.App.Controller.BonitaActivitiesFilters[key];
@@ -377,10 +382,10 @@
                                 }
                             }
                         }
-                        var req = 'SELECT category as name, COUNT(*) AS y FROM ? WHERE 1=1 '+strFilters+' GROUP BY category';
+                        var req = "SELECT category as name, COUNT(*) AS y FROM ? WHERE 1=1 "+strFilters+" AND category not in ('TIME_OFF') GROUP BY category";
                         var result = alasql(req,[$.Oda.App.Controller.BonitaActivities]);
 
-                        Highcharts.chart('divReport', {
+                        Highcharts.chart('divGraph', {
                             chart: {
                                 plotBackgroundColor: null,
                                 plotBorderWidth: null,
@@ -414,7 +419,83 @@
                         });
                         return this;
                     } catch (er) {
-                        $.Oda.Log.error("$.Oda.App.Controller.Home.displayReport : " + er.message);
+                        $.Oda.Log.error("$.Oda.App.Controller.Home.displayReportCamCategory : " + er.message);
+                        return null;
+                    }
+                },
+                /**
+                 * @returns {$.Oda.App.Controller.Home}
+                 */
+                displayTotal: function () {
+                    try {
+                        var sum = 0;
+                        var indexColl = $.Oda.App.Controller.BonitaActivitiesObjTable.entete["workHours"];
+                        var coll = $.Oda.App.Controller.BonitaActivitiesTable.column(indexColl,  { search:'applied' });
+                        var datas = coll.data();
+                        datas.each(function (d, j) {
+                            sum += parseInt(d);
+                        });
+                        var days = $.Oda.Tooling.arrondir(sum / 8,2);
+                        $('#totalDays').text(days + " MD");
+                        return this;
+                    } catch (er) {
+                        $.Oda.Log.error("$.Oda.App.Controller.Home.displayTotal : " + er.message);
+                        return null;
+                    }
+                },
+                /**
+                 * @returns {$.Oda.App.Controller.Home}
+                 */
+                displayReportBillable: function () {
+                    try {
+                        var strFilters = "";
+                        for(var key in $.Oda.App.Controller.BonitaActivitiesFilters){
+                            var filter = $.Oda.App.Controller.BonitaActivitiesFilters[key];
+                            if(filter !== ""){
+                                if(key === "customer"){
+                                    strFilters += " AND customer->displayName like '%"+filter+"%' ";
+                                }else{
+                                    strFilters += " AND "+key+" like '%"+filter+"%' ";
+                                }
+                            }
+                        }
+                        var req = "SELECT SUM(workHours) as workTime FROM ? WHERE 1=1 "+strFilters+" AND category not in ('TIME_OFF', 'TRIP', 'EXTERNAL_HELP')";
+                        var result = alasql(req,[$.Oda.App.Controller.BonitaActivities]);
+                        var workTime = result[0].workTime;
+
+                        var req = "SELECT SUM(workHours) as exp FROM ? WHERE 1=1 "+strFilters+" AND category = 'EXP'";
+                        var result = alasql(req,[$.Oda.App.Controller.BonitaActivities]);
+                        var exp = result[0].exp;
+
+                        var billable = $.Oda.Tooling.arrondir((exp / workTime) * 100, 2);
+
+                        var strHtml = $.Oda.Display.TemplateHtml.create({
+                            template : "tlpDivReportBillable",
+                            scope: {
+                                workTime: workTime,
+                                exp: exp,
+                                billable: billable
+                            }
+                        });
+                        $.Oda.Display.render({id:"divReport", html: strHtml});
+                        return this;
+                    } catch (er) {
+                        $.Oda.Log.error("$.Oda.App.Controller.Home.displayReportBillable : " + er.message);
+                        return null;
+                    }
+                },
+                /**
+                 * @returns {$.Oda.App.Controller.Home}
+                 */
+                displayReportAllExp: function () {
+                    try {
+                        var strHtml = $.Oda.Display.TemplateHtml.create({
+                            template : "tlpDivReportAllExp"
+                        });
+                        $.Oda.Display.render({id:"divReport", html: strHtml});
+                        return this;
+                    } catch (er) {
+                        $.Oda.Log.error("$.Oda.App.Controller.Home.displayReportAllExp : " + er.message);
                         return null;
                     }
                 },
